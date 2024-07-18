3379d4a92609a7ea53671a190e5afc320765e7fa
jmj0707 <139612475+jmj0707@users.noreply.github.com>
2024-07-18 17:07:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,18 +23,20 @@ if 'file_sheet_data' not in st.session_state:
 uploaded_files = st.file_uploader("캠페인 브리프 파일을 업로드하세요", type=["xlsx"], accept_multiple_files=True)
 st.write("---")
 
+save_directory = st.text_input("파일을 저장할 디렉토리를 입력하세요")
+
 def on_input_change():
     user_input = st.session_state.user_input
-    st.session_state.past.append(user_input) # past 배열에 user_input 추가
+    st.session_state.past.append(user_input)
 
     if uploaded_files:
-        combined_response, file_sheet_data = process_uploaded_file(uploaded_files, user_input)
-        st.session_state.generated.append({'type': 'normal', 'data': combined_response}) # generated 배열에 응답 추가
+        combined_response, file_sheet_data = process_uploaded_file(uploaded_files, user_input, save_directory)
+        st.session_state.generated.append({'type': 'normal', 'data': combined_response})
         st.session_state.file_sheet_data.update(file_sheet_data)
     else:
         st.session_state.generated.append({'type': 'normal', 'data': "파일을 업로드하지 않았거나, 파일 처리 중 오류가 발생했습니다."})
 
-def on_btn_click(): # 초기화
+def on_btn_click():
     del st.session_state.past[:]
     del st.session_state.generated[:]
     st.session_state.file_sheet_data = {}
@@ -59,31 +61,27 @@ with chat_placeholder.container():
 with st.container():
     st.text_input("User Input:", on_change=on_input_change, key="user_input")
 
-file_data = [] # 파일 data
+file_data = []
 total = []
 
-# 파일과 시트 데이터를 출력하는 예시
 st.write("### 업로드된 파일 및 시트 데이터")
 for file_name, sheets in st.session_state['file_sheet_data'].items():
-    file_data.append(file_name)  # 파일 이름을 file_data에 추가
-    total.append([])  # 해당 파일의 시트를 위한 빈 리스트를 total에 추가
+    file_data.append(file_name)
+    total.append([])
     for sheet_name, response in sheets.items():
-        total[-1].append([sheet_name, response])  # 최신 서브 리스트에 시트 데이터를 추가
-
-    # st.write(file_data, total)
-
+        total[-1].append([sheet_name, response])
 
 docs = []
-k= 0
-for i in range(len(total)):  # 파일의 개수
+k = 0
+for i in range(len(total)):
     temp = file_data[i]
     for j in range(len(total[i])):
         page_content = str(total[i][j][1])
-        k= k+1
+        k += 1
 
         doc = Document(
             page_content=page_content,
-            metadata={'file_name': temp, 'sheet_name': total[i][j][0], 'page' : k}
+            metadata={'file_name': temp, 'sheet_name': total[i][j][0], 'page': k}
         )
         docs.append(doc)
 
